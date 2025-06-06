2323b2179ccd02a4215099b006017cf3d3dff78e
srimathis1 <srimathis087@gmail.com>
2025-06-06 22:25:08 +0530

    Update db_config.py

--- musicdbmsproj/db_config.py
+++ musicdbmsproj/db_config.py
@@ -1,16 +1,16 @@
-# db_config.py
+import os
 import oracledb
 
-# Oracle connection details
-username = "SYSTEM"
-password = "system"
-dsn = "localhost:1522/orcl"
-
-# Create and return a connection
 def get_connection():
     try:
-        connection = oracledb.connect(user=username, password=password, dsn=dsn)
-        return connection
+        # Get credentials from environment variables (Render-safe)
+        username = os.environ.get("SYSTEM")
+        password = os.environ.get("system")
+        dsn = os.environ.get("localhost:1522/orcl")  # Format: host:port/service_name
+
+        # Connect
+        conn = oracledb.connect(user=username, password=password, dsn=dsn)
+        return conn
     except oracledb.DatabaseError as e:
         print("Oracle connection error:", e)
         return None
